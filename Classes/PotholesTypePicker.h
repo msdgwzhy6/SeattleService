7cc69f0018e5f0f0570acaf8af6b7e54301b6397
JD Wallace <jd@jdwallace.com>
2010-01-18 20:48:44 -0800

    Added Picker for Potholes Type but not wired up.

---
+++ Classes/PotholesTypePicker.h
@@ -0,0 +1,16 @@
+//
+//  PotholesTypePicker.h
+//  SeattleService
+//
+//  Created by JD Wallace on 1/18/10.
+//  Copyright 2010 jdwallace.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PotholesTypePicker : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource, UIActionSheetDelegate> {
+
+}
+
+@end
